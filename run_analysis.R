bb105b3aa1f23c7d34e4671d468eea31b7a093e5
nitish <nitish.vashishtha@guavus.com>
2014-05-24 19:53:23 +0530

    Updated script with final tidy data computation

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,6 @@
+#
 #Specify file names for all relevant data files
+#
 features.file <- "./UCI HAR Dataset/features.txt"
 activity.label.file <- "./UCI HAR Dataset/activity_labels.txt"
 
@@ -10,7 +12,9 @@ test.set.file <- "./UCI HAR Dataset/test/X_test.txt"
 test.subject.file <- "./UCI HAR Dataset/test/subject_test.txt"
 test.activity.file <- "./UCI HAR Dataset/test/y_test.txt"
 
+#
 #Load data into R objects from file
+#
 features.df <- read.table(file=features.file)
 activity.df <- read.table(file=activity.label.file)
 
@@ -22,9 +26,14 @@ test.df <- read.table(file=test.set.file)
 test.subject.df <- read.table(file=test.subject.file)
 test.activity.df <- read.table(file=test.activity.file)
 
+#
 #Set appropriate column names for data sets
+#
 colnames(features.df) <- c('feature.id', 'feature.label')
 colnames(activity.df) <- c('activity.id', 'activity.label')
+
+features.df[,2] <- sapply( features.df[,2], function(arg) { arg = gsub(arg, pattern='\\(\\)', replacement='');arg = gsub(arg, pattern='[[:punct:]]', replacement='.') } )
+activity.df[,2] <- sapply( activity.df[,2], function(arg) { arg = tolower(gsub(arg, pattern='\\(\\)', replacement=''));arg = gsub(arg, pattern='[[:punct:]]', replacement='.') } )
 
 colnames(train.df) <- features.df[,2]
 colnames(train.subject.df) <- 'subject.id'
@@ -34,11 +43,15 @@ colnames(test.df) <- features.df[,2]
 colnames(test.subject.df) <- 'subject.id'
 colnames(test.activity.df) <- 'activity.id'
 
+#
 #Extract features with mean and standard deviation measurements
+#
 train.df <- subset(train.df, select= grep(colnames(train.df), pattern='mean|std', ignore.case=F))
 test.df <- subset(test.df, select= grep(colnames(test.df), pattern='mean|std', ignore.case=F))
 
-Merge 'subject' and 'activity' data with 'feature' data
+#
+#Merge 'subject' and 'activity' metadata with 'feature' data
+#
 train.df <- cbind(train.activity.df, train.df)
 train.df <- cbind(train.subject.df, train.df)
 colnames(train.df)[1] <- 'subject.id'
@@ -49,15 +62,27 @@ test.df <- cbind(test.subject.df, test.df)
 colnames(test.df)[1] <- 'subject.id'
 colnames(test.df)[2] <- 'activity.id'
 
-
+#
 #Merge test and training datasets
+#
 final.df <- rbind(train.df, test.df)
 
+#
 #Add activity labels
-suppressWarnings(final.df <- merge(final.df, activity.df, by='activity.id', sort=F))
-#Remove activity ids
-final.df <- subset(final.df, select = -c(1))
-#Reorder columns to get activity label before features
-final.df <- subset(final.df, select = c(1, 81, 2:80))
+#
+suppressWarnings(final.df <- merge(activity.df, final.df, by='activity.id', sort=F))
 
-final.df
+#
+#Remove activity id column, put subject id as first column
+#
+final.df <- subset(final.df, select = c(3, 2, 4:ncol(final.df)))
+
+#
+#Aggregate data for unique combinations of the subject and activity features
+#
+tidy.data <- aggregate( final.df[,3:ncol(final.df)], final.df[,1:2], FUN=mean )
+
+#
+#Return
+#
+tidy.data
